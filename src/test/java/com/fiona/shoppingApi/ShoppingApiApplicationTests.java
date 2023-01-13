0a816d5b035bc9d106d3e7e7a8f9ee14d35e8837
fMurugi <fionamurugi99@gmail.com>
2023-01-13 14:21:07 +0300

    initial commit

---
+++ src/test/java/com/fiona/shoppingApi/ShoppingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiona.shoppingApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
